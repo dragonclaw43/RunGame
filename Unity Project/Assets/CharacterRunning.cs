d95e3264578c1b62e63061997cf6a42e74c3cb9a
Kevin <dahornnpt@gmail.com>
2013-11-27 20:42:10 -0500

    Added Unity Project
    Added project, also added Ester (our new human placeholder) and I have
    ester running forward and jumping with the space bar

---
+++ Unity Project/Assets/CharacterRunning.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterRunning : MonoBehaviour {
+	bool colliding = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!colliding){
+			transform.Translate(Vector3.down);
+		}
+	}
+
+	void OnCollisionEnter(Collision theCollision){
+		if(theCollision.collider.gameObject.name == "Wall"){
+			colliding = true;
+		}
+		else{
+			colliding = false;
+		}
+	}
+}

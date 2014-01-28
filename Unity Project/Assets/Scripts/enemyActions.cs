e64de58fdf762da2b6121bbb16e71469712bd1ec
Joseph Snyder <joseph.snyder0@gmail.com>
2014-01-27 21:51:10 -0500

    Update enemyActions script
    Add wait to patrolling enemy, have a public int to determine how many
    frames the enemy will wait
    
    Add chase enemy, which starts chase at some set distance and chases
    until robber is too far away.  Add attack function if robber is between
    2 and 6 units away for chasers.  (Should expand to patrol units too)
    
    Add a bool to determine what type of attack is used.  If enemy is
    mobster class, it will attempt to damage the robber.  if not, the enemy
    will attempt to tackle the robber.

--- Unity Project/Assets/Scripts/enemyActions.cs
+++ Unity Project/Assets/Scripts/enemyActions.cs
@@ -4,8 +4,13 @@ using System.Collections;
 public class enemyActions : MonoBehaviour {
 
 	public int moveLimit = 60;
+	public int waitLimit = 15;
 	public float enemySpeed = .3f;
-	public bool isMoving = true;
+	public float chaseSpeed = 7;
+	public float chaseDistance = 15;
+	public bool isMoving = false;
+	public bool isChase = false;
+	public bool isMobster= false;
 	int enemyHealth = 1;
 	int moveCount = 0;
 	Vector3 enemyVelocity;
@@ -22,15 +27,42 @@ public class enemyActions : MonoBehaviour {
 	// a non-moving enemy will not turn at all
 	void Update () {
 		if(isMoving){
-			transform.Translate(enemyVelocity);
 			moveCount++;
 			if(moveCount > moveLimit){
-				transform.Rotate(new Vector3(0,180,0));
-				moveCount=0;
+				transform.Translate(new Vector3(0,0,0));
+				if(moveCount >  moveLimit + waitLimit) {
+					transform.Rotate(new Vector3(0,180,0));
+					moveCount=0;
+			    }
+			}
+			else {
+				transform.Translate(enemyVelocity);
 			}
 		}
+		else if (isChase) {
+			// Chase code stolen from : http://forum.unity3d.com/threads/34046-Follow-Chase-Player
+			float range = Vector3.Distance(transform.position,GameObject.Find("Robber").transform.position);
+			if(range < chaseDistance){
+				transform.LookAt(GameObject.Find("Robber").transform);
+			    transform.Translate(chaseSpeed*Vector3.forward*Time.deltaTime);
+				if (range > 2 & range < 6){
+					attack();
+				}
+			}
+		} 
 	}
 
+	void attack() {
+
+		if(isMobster){
+			//animation.play("attack")
+			Debug.Log("Mob Attack!");
+		}
+		else {
+			//animation.play("Tackle")
+			Debug.Log("Tackle!");
+		}
+	}
 	// Enemy health goes down on collision for now
 	// Not quite sure how punch will be implemented.
 	// eventually will need a contact point to see if enemy is hit on weak side??
